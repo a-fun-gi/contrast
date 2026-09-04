23cde2a2a2194f6c4d367a9c242c7ea8921a82b4
a-fun-gi <175504279+a-fun-gi@users.noreply.github.com>
2026-09-04 16:05:11 -0700

    added better terrain and moving squares

--- godot/scenes/player_2.gd
+++ godot/scenes/player_2.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -700.0
 
 func _ready() -> void:
 	print("!!! THE SCRIPT IS ALIVE !!!")
@@ -15,12 +15,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("player_left", "player_right")
 	if direction > 0:
 		visuals.scale.x = 1
 	elif direction < 0:
